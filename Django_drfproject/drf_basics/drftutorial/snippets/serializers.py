f24a777e000d414feecb5f66d28404ec1246a5db
윤서아 <yoonseoah@yunseoaui-MacBookAir.local>
2023-06-29 19:20:51 +0900

    윤서아: 230629

---
+++ Django_drfproject/drf_basics/drftutorial/snippets/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+
+    def create(self, validated_data):
+        return Snippet.objects.create(**validated_data)
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.code = validated_data.get('code', instance.code)
+        instance.linenos = validated_data.get('linenos', instance.linenos)
+        instance.language = validated_data.get('language', instance.language)
+        instance.style = validated_data.get('style', instance.style)
+        instance.save()
+        return instance
---
+++ Django_drfproject/drfproject/drfblogproject/api/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    email = models.EmailField(max_length=100, unique=True)
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comment')
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+# Create your models here.
